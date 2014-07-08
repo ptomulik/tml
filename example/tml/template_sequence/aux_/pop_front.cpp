4abf875b87bfc7387f36f757e27c97d5586aacc6
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2014-07-08 21:43:12 +0200

    fixed example/tml/template_sequence/aux_/pop_front.cpp

--- example/tml/template_sequence/aux_/pop_front.cpp
+++ example/tml/template_sequence/aux_/pop_front.cpp
@@ -4,16 +4,16 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
 
-/** // doc: tml/template_sequence/aux_/pop_back.cpp {{{
- * \file tml/template_sequence/aux_/pop_back.cpp
- * \example tml/template_sequence/aux_/pop_back.cpp
- * \brief Usage example for tml::pop_back_impl<aux::template_sequence_tag>
+/** // doc: tml/template_sequence/aux_/pop_front.cpp {{{
+ * \file tml/template_sequence/aux_/pop_front.cpp
+ * \example tml/template_sequence/aux_/pop_front.cpp
+ * \brief Usage example for tml::pop_front_impl<aux::template_sequence_tag>
  */ // }}}
 
 // [Code]
-#include <tml/pop_back.hpp>
+#include <tml/pop_front.hpp>
 #include <tml/template_sequence/aux_/tag.hpp>
-#include <tml/template_sequence/aux_/pop_back.hpp>
+#include <tml/template_sequence/aux_/pop_front.hpp>
 
 using namespace tml;
 
@@ -21,10 +21,10 @@ template <class...> struct seq { typedef aux::template_sequence_tag sequence_tag
 
 typedef seq<char,short,int,float> s1;
 
-typedef pop_back<s1>::type r1;
+typedef pop_front<s1>::type r1;
 
 #include <tml/is_same.hpp>
-static_assert(is_same<r1,seq<char,short,int> >::value, "");
+static_assert(is_same<r1,seq<short,int,float> >::value, "");
 // [Code]
 
 int main() { return 0; }
